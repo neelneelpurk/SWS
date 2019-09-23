d33c16cb7f5a7a25107fad181dfeab17bd2a1f45
neelneelpurk <soumyadeep1999.np@gmail.com>
2019-09-23 17:45:03 +0530

    added comments

--- 1_mr_kim.cpp
+++ 1_mr_kim.cpp
@@ -5,11 +5,11 @@
 
 using namespace std;
 
-int N;
-int sx,sy;
-int dx,dy;
-int cusX[12],cusY[12];
-int vis[12];
+int N; 		//number of deliveries
+int sx,sy; 	//source or office
+int dx,dy; 	//destination or home
+int cusX[12],cusY[12]; 	//delivery addresses
+int vis[12];	//visited array for Recursion
 int ans;
 int ABS(int aa)
 {
@@ -22,15 +22,15 @@ int ABS(int aa)
 void solve(int xx, int yy, int visited, int tot_cost, int idx){
     int cost = 0;
     if(idx!=-1) vis[idx]=1;
-    if(visited==N){
-        tot_cost += ABS(xx-dx) + ABS(yy-dy);
+    if(visited==N){				//if all deliveries done
+        tot_cost += ABS(xx-dx) + ABS(yy-dy);	// count cost from last delivery to home
         ans = min(ans,tot_cost);
         return;
     }
     for(int i=0;i<N;i++){
         if(!vis[i]){
             cost = tot_cost + ABS(xx-cusX[i]) + ABS(yy-cusY[i]);
-            solve(cusX[i],cusY[i],visited+1,cost,i);
+            solve(cusX[i],cusY[i],visited+1,cost,i);	//set 'i' as delivered
             vis[i]=0;
         }
     }
